68aab3086d421aeaeb3e2cd0b4842b38a985e0e7
haseebnaeem94 <162363763+haseebnaeem94@users.noreply.github.com>
2025-05-30 17:29:26 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="border-t">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          H-Tech Blogs. All rights reserved.
+          Haseeb-Tech Blogs. All rights reserved.
         </div>
       </div>
     </footer>
